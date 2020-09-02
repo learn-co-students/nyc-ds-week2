c843f8e55feec859add08bb62b5cfe8e2aa6ad44
joelsewhere <joelsewhere@gmail.com>
2020-09-02 17:54:56 -0500

    initial commit

---
+++ src/create_db.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import sqlite3
+import os
+parent_directory = os.path.dirname(os.getcwd())
+
+
+# ========================== CREATE TABLES =========================
+db_path = os.path.join(parent_directory, 'data', 'stocks.db')
+conn = sqlite3.connect(db_path)
+
+conn.execute('''DROP TABLE IF EXISTS date;''')
+conn.execute('''
+            CREATE TABLE date 
+                (id INTEGER PRIMARY KEY,
+                date DATE NOT NULL);''')
+
+conn.execute('''DROP TABLE IF EXISTS company;''')
+conn.execute('''
+            CREATE TABLE company 
+                (id INTEGER PRIMARY KEY,
+                name VARCHAR NOT NULL UNIQUE);''')
+
+conn.execute('''DROP TABLE IF EXISTS open;''')
+conn.execute('''
+            CREATE TABLE open (
+                id INTEGER PRIMARY KEY,
+                open DECIMAL NOT NULL,
+                company_id VARCHAR,
+                date_id VARCHAR,
+                CONSTRAINT company_id
+                    FOREIGN KEY (company_id)
+                    REFERENCES company(id)
+                CONSTRAINT date_id
+                    FOREIGN KEY (date_id)
+                    REFERENCES date(id));''')
+
+# ========================== IMPORT DATASET =========================
+
+stocks_dataset_path = os.path.join(parent_directory,'data','all_stocks_5yr.csv')
+df = pd.read_csv(stocks_dataset_path)
+df.date = pd.to_datetime(df.date)
+df.sort_values(by='date', inplace=True)
+
+# ========================= MOVE DATASET TO DB ===================
+
+# DATE TABLE
+df[['date']]\
+    .drop_duplicates()\
+    .to_sql('date', conn, index=False, if_exists='append')
+
+# Collect date unique ids
+dates = conn.execute('''select * from date''').fetchall()
+date_map = {}
+for date in dates:
+    converted = pd.to_datetime(date[1])
+    date_map[converted] = date[0]
+
+# COMPANY TABLE
+df[['Name']]\
+    .drop_duplicates()\
+    .to_sql('company', conn, index=False, if_exists='append')
+
+# Collect company unique ids
+companies = conn.execute('''select * from company''').fetchall()
+company_map = {}
+for company in companies:
+    converted = company[1]
+    company_map[converted] = company[0]
+
+# OPEN TABLE
+open_table = df[['open', 'date', 'Name']]
+open_table['date_id'] = open_table.date.apply(lambda x: date_map[x])
+open_table['company_id'] = open_table.Name.apply(lambda x: company_map[x])
+
+open_table[['open', 'date_id', 'company_id']]\
+    .dropna()\
+    .to_sql('open', conn, 
+            index=False, if_exists='append')
